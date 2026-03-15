bb7286d6a1d53465f14c5fda73539ef3b8f3bd94
deepak <deepak@singsys.com>
2026-03-15 20:09:45 +0530

    chnges

--- src/app.ts
+++ src/app.ts
@@ -12,22 +12,61 @@ import { prisma } from "./config/prisma";
 import "./utils/json_polyfill";
 
 export const app = express();
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "script-src": ["'self'", "https://cdn.jsdelivr.net", "https://kit.fontawesome.com", "https://ka-f.fontawesome.com"],
-      "script-src-elem": ["'self'", "https://cdn.jsdelivr.net", "https://kit.fontawesome.com", "https://ka-f.fontawesome.com"],
-      "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://ka-f.fontawesome.com"],
-      "font-src": ["'self'", "https://fonts.gstatic.com", "https://ka-f.fontawesome.com"],
-      "connect-src": ["'self'", "https://ka-f.fontawesome.com"],
-      "img-src": ["'self'", "data:", "https://*"]
+app.set("trust proxy", 1);
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+
+        scriptSrc: [
+          "'self'",
+          "https://cdn.jsdelivr.net",
+          "https://kit.fontawesome.com",
+          "https://ka-f.fontawesome.com"
+        ],
+
+        scriptSrcElem: [
+          "'self'",
+          "https://cdn.jsdelivr.net",
+          "https://kit.fontawesome.com",
+          "https://ka-f.fontawesome.com"
+        ],
+
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "https://fonts.googleapis.com"
+        ],
+
+        fontSrc: [
+          "'self'",
+          "https://fonts.gstatic.com"
+        ],
+
+        imgSrc: [
+          "'self'",
+          "data:",
+          "https:"
+        ],
+
+        connectSrc: [
+          "'self'",
+          "https://cdn.jsdelivr.net",
+          "https://ka-f.fontawesome.com"
+        ],
+
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
     },
-  },
-}));
+  })
+);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 
 
@@ -43,7 +82,6 @@ app.use("/api/v1/urls", urlRoutes);
 app.use("/api/v1/analytics", analyticsRoutes);
 app.get("/:shortCode", redirectToLongUrl);
 
-app.set("trust proxy", 1);
 
 process.on("SIGTERM", async () => {
   await prisma.$disconnect();
